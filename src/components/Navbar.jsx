ec70c0482643e6efd35ef7a502316bf538ed1335
Brendon <brendonpjon@gmail.com>
2024-02-29 13:48:53 +0000

    Cleaned up UI and bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Link } from "react-router-dom"
+import { NavLink} from "react-router-dom"
 import top from "../assets/top.svg"
 import action from "../assets/action.svg"
 import adventure from "../assets/adventure.svg"
@@ -12,11 +12,11 @@ import sliceOfLife from "../assets/slice.svg"
 
 function Navbar() {
 
-    const [genre,setGenre] = useState([{id: 0, icon: top, name: "Top Anime", active: true},{ id: 1, icon: action, name: "Action", active:false },{id: 2, icon: adventure, name: "Adventure", active:false},{id:4, icon: comedy, name: "Comedy", active:false},{id:8, icon: drama, name: "Drama" , active:false},{ id:10, icon: fantasy, name: "Fantasy", active:false},{ id:14, icon: horror, name: "Horror", active:false},{ id:22, icon: romance, name: "Romance", active:false},{ id:36, icon: sliceOfLife, name: "Slice of Life" ,active:false}])
+    const [genre,setGenre] = useState([{id: 0, icon: top, name: "Top Anime", active: true, link: "/"},{ id: 1, icon: action, name: "Action", active:false, link: "/Genre/1" },{id: 2, icon: adventure, name: "Adventure", active:false, link: "/Genre/2"},{id:4, icon: comedy, name: "Comedy", active:false, link: "/Genre/4"},{id:8, icon: drama, name: "Drama" , active:false, link: "/Genre/8"},{ id:10, icon: fantasy, name: "Fantasy", active:false, link: "/Genre/10"},{ id:14, icon: horror, name: "Horror", active:false, link: "/Genre/14"},{ id:22, icon: romance, name: "Romance", active:false, link: "/Genre/22"},{ id:36, icon: sliceOfLife, name: "Slice of Life" ,active:false, link: "/Genre/36"}])
       return (
             <div className="navbar-section">
                     {genre.map((g) => (
-                       <Link to={`/Genre/${g.id}`} key={g.id} ><button className={g.active ? "genre genre-active" : "genre"}><img className="sidebar-icons" src={g.icon} width={25} height={25} alt="" />{g.name}</button></Link>
+                       <NavLink to={g.link} className={({isActive}) => isActive ? g.active = true : g.active = false } key={g.id} ><button className={g.active ? "genre genre-active" : "genre"}><img className="sidebar-icons" src={g.icon} width={25} height={25} alt="" />{g.name}</button></NavLink>
                     ))}
             </div>
       )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import top from "../assets/top.svg"
 import action from "../assets/action.svg"
 import adventure from "../assets/adventure.svg"
@@ -13,7 +13,7 @@ import x from "../assets/x.svg"
 
 function Sidebar({setDrawer, drawer}) {
 
-    const [genre,setGenre] = useState([{id: 0, icon: top, name: "Top Anime", active: true},{ id: 1, icon: action, name: "Action", active:false },{id: 2, icon: adventure, name: "Adventure", active:false},{id:4, icon: comedy, name: "Comedy", active:false},{id:8, icon: drama, name: "Drama" , active:false},{ id:10, icon: fantasy, name: "Fantasy", active:false},{ id:14, icon: horror, name: "Horror", active:false},{ id:22, icon: romance, name: "Romance", active:false},{ id:36, icon: sliceOfLife, name: "Slice of Life" ,active:false}])
+    const [genre,setGenre] = useState([{id: 0, icon: top, name: "Top Anime", active: true, link: "/"},{ id: 1, icon: action, name: "Action", active:false, link: "/Genre/1" },{id: 2, icon: adventure, name: "Adventure", active:false, link: "/Genre/2"},{id:4, icon: comedy, name: "Comedy", active:false, link: "/Genre/4"},{id:8, icon: drama, name: "Drama" , active:false, link: "/Genre/8"},{ id:10, icon: fantasy, name: "Fantasy", active:false, link: "/Genre/10"},{ id:14, icon: horror, name: "Horror", active:false, link: "/Genre/14"},{ id:22, icon: romance, name: "Romance", active:false, link: "/Genre/22"},{ id:36, icon: sliceOfLife, name: "Slice of Life" ,active:false, link: "/Genre/36"}])
   return (
     <div className={drawer ? "sidebar-black-card sidebar-black-card-active" : "sidebar-black-card sidebar-black-card-closed"}>
         <div className={drawer ? "sidebar sidebar-active" : "sidebar sidebar-closed"}>
@@ -23,7 +23,7 @@ function Sidebar({setDrawer, drawer}) {
         <div>
         {
             genre.map((g) => (
-                <Link to={`/Genre/${g.id}`} key={g.id}><button  className={g.active ? "genre genre-active" : "genre"}><img className="sidebar-icons" src={g.icon} width={25} height={25} alt="" />{g.name}</button> </Link>
+                <NavLink to={g.link} key={g.id} className={({isActive}) => isActive ? g.active = true : g.active = false }><button  className={g.active ? "genre genre-active" : "genre"}><img className="sidebar-icons" src={g.icon} width={25} height={25} alt="" />{g.name}</button> </NavLink>
             ))
         }
         </div>
